21b0fb59894be1eb2952eb7e33ad6f5ed0d914b7
Gauri <gauribansal1612@gmail.com>
2026-07-20 04:04:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
+const passwordEl = document.getElementById("password");
+const generateBtn = document.getElementById("generate-btn");
+const copyBtn = document.getElementById("copy-btn");
+
+
+
 let character = "ASDFGHJKLPOIUYTREWQZXCVBNMzxcvbnmlkjhgfdsaqwertyuiop1234567890!@#$%^&*()"
 
 let plength = 8;
 
 let password = ""
 
-for (let i = 0; i < plength; i++) {
-    let rendomIndex = Math.floor(Math.random() * character.length)
 
-    password += character[rendomIndex]
+function generatePassword() {
+
+    password = "";
+    for (let i = 0; i < plength; i++) {
+        let rendomIndex = Math.floor(Math.random() * character.length)
+        password += character[rendomIndex]
+    }
+
+    passwordEl.textContent = password
+    console.log(password)
 }
-console.log(password)
+// background: #10B981;
+//  background: #1F2937;
+
+generateBtn.addEventListener("click", function() {
+    // password generate function call
+    generatePassword();
+});
+
+copyBtn.addEventListener("click", function() {
+    navigator.clipboard.writeText(passwordEl.textContent);
+
+    copyBtn.textContent = "✅ Copied!";
+    copyBtn.classList.add("copied");
+
+    setTimeout(function() {
+        copyBtn.textContent = "Copy";
+        copyBtn.classList.remove("copied");
+    }, 2000);
+});
+
+// copyBtn.addEventListener("click", function() {
+//     navigator.clipboard.writeText(passwordEl.textContent);
+//     alert("✅ Password copied!");
+// });
